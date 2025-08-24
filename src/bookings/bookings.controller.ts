f04ed853904e04049e775b472e6e5e95cc8637b6
Your Name <you@example.com>
2025-08-24 11:07:02 +0700

    fix: add JwtAuthGuard to BookingsController for route protection

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, InternalServerErrorException, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, InternalServerErrorException, NotFoundException, BadRequestException, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiBearerAuth, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
 import { CreateBookingDto } from './dto/req/create-booking.dto';
 import { UpdateBookingDto } from './dto/req/update-booking.dto';
 import { CreateBookingResDto } from './dto/res/create-booking-res.dto';
 import { UpdateBookingResDto } from './dto/res/update-booking-res.dto';
 import { BookingsService } from './bookings.service';
+import { JwtAuthGuard } from 'src/global/jwt-auth.guard';
 
 @ApiTags('Bookings')
 @ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('bookings')
 export class BookingsController {
     constructor(private readonly bookingsService: BookingsService) {}
